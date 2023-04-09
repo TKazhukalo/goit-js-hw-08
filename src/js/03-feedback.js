ec7c29185d33bd59054ea5c49198291ac0d2dbb2
TKazhukalo <118135022+TKazhukalo@users.noreply.github.com>
2023-04-09 20:28:07 +0300

    update-tast-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,8 @@
 
 import throttle from "lodash.throttle";
-const STOREGE_KEY = 'feedback - form - state';
+const STORAGE_KEY = 'feedback - form - state';
 
-const formData = {};
+let formData = {};
 const formEl = document.querySelector('.feedback-form');
 const messageEl = document.querySelector('.feedback-form textarea');
 const inputEl = document.querySelector('.feedback-form input');
@@ -16,29 +16,34 @@ formEl.addEventListener('input', throttle(onTextareaInput,500));
    // console.log(formDataParse);
    
 })*/
+
 populateFormData();
 function onFormSubmit(evt) {
     evt.preventDefault();
     if (inputEl.value === '' || messageEl.value === '') {
 return alert( 'Введіть дані');
   }
-    console.log(JSON.parse(localStorage.getItem(STOREGE_KEY)));
-
-    localStorage.removeItem(STOREGE_KEY);
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+   
+    localStorage.removeItem(STORAGE_KEY);
         evt.currentTarget.reset();
 }
 
 function onTextareaInput(evt) {
- formData[evt.target.name] = evt.target.value;
-     
-    localStorage.setItem(STOREGE_KEY, JSON.stringify(formData));
+ //formData[evt.target.name] = evt.target.value.trim();
+    formData = {
+    email: inputEl.value,
+        message: messageEl.value
+    }  
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 
 function populateFormData() {
-    const savedMessage = JSON.parse(localStorage.getItem(STOREGE_KEY));
+ const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
     if (savedMessage) {
         messageEl.value = savedMessage.message || '';
         inputEl.value = savedMessage.email || '';
     }
+ 
 }
